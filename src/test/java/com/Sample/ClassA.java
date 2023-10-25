0c46ca0f3ff7c11b675356d4bb28d3ea61b1a54b
umagitclass <umarajasekar@outlook.com>
2023-10-25 21:07:46 +0530

    First time push

---
+++ src/test/java/com/Sample/ClassA.java
@@ -0,0 +1,15 @@
+package com.Sample;
+
+public class ClassA {
+	
+	private void m1() {
+		System.out.println("1");
+	}
+	private void m2() {
+		System.out.println("2");
+	}
+	private void m3() {
+		System.out.println("3");
+	}
+
+}
