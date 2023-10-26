fde2c6200df3a7e829dca73c6600b6a36b208c3c
umagitclass <umarajasekar@outlook.com>
2023-10-26 11:18:50 +0530

    Added new methods

--- src/test/java/com/Sample/ClassA.java
+++ src/test/java/com/Sample/ClassA.java
@@ -11,5 +11,14 @@ public class ClassA {
 	private void m3() {
 		System.out.println("3");
 	}
+	private void m4() {
+		System.out.println("4");
+	}
+	private void m5() {
+		System.out.println("5");
+	}
+	private void m6() {
+		System.out.println("6");
+	}
 
 }
